afdb5d522f29965a1717edb6afceb22486a88237
et-zone <2250219329@qq.com>
2021-06-13 17:44:03 +0800

    test

--- cache_test.go
+++ cache_test.go
@@ -3,11 +3,12 @@ package lcache
 import (
 	"fmt"
 	"testing"
+	// "time"
 )
 
 func Test_run(t *testing.T) {
 	cache := NewLocalCache()
-	cache.Set("aa", "aaaa", 0)
+	cache.Set("aa", "aaaa", 2)
 	cache.Set("bb", "bbbb", 0)
 	cache.Set("cc", "cccc", 0)
 
@@ -17,4 +18,9 @@ func Test_run(t *testing.T) {
 	fmt.Println(d)
 	d, _ = cache.Get("cc")
 	fmt.Println(d)
+
+	// time.Sleep(time.Secosnd * 3)
+	// d, _ = cache.Get("aa")
+	// fmt.Println(d)
+
 }
